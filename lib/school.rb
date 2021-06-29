ca76bd40c2205a81b723ba5bdfb24327ec340985
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-29 16:13:49 -0400

    commit tree 374caa89e92698ffd2153c3d60de136882623c20

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+            @roster[grade] << name
+        else
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade,name|
+            newArray = @roster[grade].sort_by{|name|name}
+            @roster[grade] = newArray
+        end
+    end
+
+
+end
+
+
